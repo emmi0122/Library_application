6c0f011031793376236a75cbdc8486e2c5d9a3d5
emmi0122 <emiliaidajarleback@gmail.com>
2025-11-06 13:19:23 +0100

    task-3

---
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UtilsTest.java
@@ -0,0 +1,46 @@
+package se.yrgo.libraryapp.validators;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class UtilsTest {
+    @ParameterizedTest
+    @CsvSource({
+        "l0va, lva",
+        "@boo, boo",
+        "!donkey, donkey",
+        "bla <>blus, bla blus",
+        "Laban😂, Laban",
+        "#'+?=^b,  b",
+    })
+    void onlyLettersAndWhitespace(String input, String expected) {
+        assertEquals(Utils.onlyLettersAndWhitespace(input), Utils.onlyLettersAndWhitespace(expected));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "l0va, lova",
+        "@boo, boo",
+        "!<>blus bla, blus bla"
+    })
+    void cleanAndUnLeet(String input, String expected) {
+        assertEquals(Utils.cleanAndUnLeet(input), Utils.cleanAndUnLeet(expected));
+    }
+}
+
+// Metoden: onlyLettersAndWhitespace
+// Remove any non-alphabetic letters from a string, but keep any whitespace.
+// Will return the string as all lowecase.
+
+// Metoden: cleanAndUnLeet
+// Converts any "leet speak" letters into their alphabetic equivalent (i.e. 4 to a etc.) and 
+// then removes any letters that are not alphabetic (but not whitespace). Will return the string
+// as all lowecase.
+
+// Två metoder behöver testas
+// Dessa metoder är package-private, inga problem så länge man använder rätt paketstruktur
+
+// Tänk igenom grundfall, randvillkor och specialfall
+// Använd @ValueSource med flera
